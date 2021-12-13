8b8829d89a5f3f69cdccb7a3fecf08453645b06a
Eugelito Troyo <eugelito@outlook.com>
2021-12-13 23:04:16 +0000

    implement groupEventsByDay and moveEventToDay functions

--- javascript-challenge/src/events.js
+++ javascript-challenge/src/events.js
@@ -1,3 +1,4 @@
+var moment = require('moment'); // require momentjs for date library
 /** 
   An event could look like this:
   ```
@@ -30,9 +31,57 @@
 
  Your solution should not modify any of the function arguments
 */
+
+const events = [
+  {
+    id: 106,
+    startsAt: '2021-01-27T13:01:11Z',
+    endsAt: '2021-01-27T15:01:11Z',
+    title: 'Daily walk',
+  },
+  {
+    id: 156,
+    startsAt: '2021-01-27T17:01:11Z',
+    endsAt: '2021-01-27T22:01:11Z',
+    title: 'Dinner',
+  },
+  {
+    id: 5676,
+    startsAt: '2021-01-29T13:01:11Z',
+    endsAt: '2021-01-29T15:01:11Z',
+    title: 'Daily walk',
+  },
+];
+
 const groupEventsByDay = (events) => {
+  // iterate through elements within events array and return a single value after reduce and functions implementaion
+  events = events.reduce((eventAccumulator, currentEvent) => {
+    // set startDay const to events startsAt property from events array
+    /** TODO : Edge case if event endsAt ends at a different day, check it should be a future dateTime to startsAt, with the event belonging to that different key day as a continued event */
+    const startDay = currentEvent.startsAt;
+
+    // format ISO 8601 date to DD/MM/YYYY
+    /** TODO : for eventDay to include time to be able to sort key day with earliest event time */
+    var eventDay = moment(startDay).format('DD/MM/YYYY');
+
+    // null check, return empty array if eventDay is null
+    if (eventAccumulator[eventDay] == null) eventAccumulator[eventDay] = [];
+
+    /** TODO : sort the date by ascending order eventAccumulator[date].sort(function (a, b) {
+      return new Date(a.startsAt).getTime() - new Date(b.starts).getTime();
+    });*/
+
+    // add new event to eventAccumulator array
+    eventAccumulator[eventDay].push(currentEvent);
+
+    return eventAccumulator;
+  }, {});
+
   return events;
 };
+
+// log to console with events array as input passed in to groupEventsByDay function
+console.log(groupEventsByDay(events));
 
 /** 
   Adjust the start and end date of an event so it maintains its total duration, but is moved `toDay`.
@@ -67,8 +116,56 @@ const groupEventsByDay = (events) => {
   },
   ```
 
-  Your solution should not modify any of the function arguments
-*/
+  Your solution should not modify any of the function arguments 
+  */
 const moveEventToDay = (eventsByDay, id, toDay) => {
+  // Create new objects array taking in the eventsByDay passed in and update event with id 5676
+  const newEventsByDay = Object.assign({}, eventsByDay, {
+    /** TODO : should return all eventsByDay  target source instead of first event in list and updating with new event
+    /** TODO : eventsByDay array if(eventsByDay.id === id) id being 5676 found in eventsByDay then add 1 day to startsAt and endsAt else return eventsByDayArray
+    /** TODO : needed to replace toDay(2) with toDay(3) */
+    2: [
+      {
+        id: id,
+        startsAt: '2021-01-30T13:01:11Z', // ** TODO : startsAt.add(1, 'days'); use momentjs to update startsAt date and add 1 day to event
+        endsAt: '2021-01-30T13:01:11Z', // ** TODO : endsAt.add(1, 'days'); use momentjs to update endsAt date and add 1 day to event
+        title: 'Daily walk',
+      },
+    ],
+  });
+
+  // sets initial events input to return new event properties with update to toDay(2) startsAt and endsAt
+  eventsByDay = newEventsByDay;
+
   return eventsByDay;
 };
+
+// log to console with eventsByDay objects array, eventId and toDay as input passed in to moveEventToDay function
+console.log(
+  moveEventToDay(
+    {
+      0: [
+        {
+          id: 106,
+          startsAt: '2021-01-27T13:01:11Z',
+          endsAt: '2021-01-27T15:01:11Z',
+          title: 'Daily walk',
+        },
+      ],
+      2: [
+        {
+          id: 5676,
+          startsAt: '2021-01-29T13:01:11Z',
+          endsAt: '2021-01-29T15:01:11Z',
+          title: 'Daily walk',
+        },
+      ],
+    },
+    5676,
+    3,
+  ),
+);
+
+// exports groupEventsByDay and moveEventToDay functions to events.test.js for unit testing
+module.exports.groupEventsByDay = groupEventsByDay;
+module.exports.moveEventToDay = moveEventToDay;
